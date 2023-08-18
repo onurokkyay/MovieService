bf828b9553424187c153031e8532a51e290bf565
onurokkyay <onurokyay1@gmail.com>
2023-08-18 14:42:31 +0300

    ExternalMovieServiceImplTest - testDiscoverMovieSuccess

--- src/test/java/com/krawen/movieservice/external/service/impl/ExternalMovieServiceImplTest.java
+++ src/test/java/com/krawen/movieservice/external/service/impl/ExternalMovieServiceImplTest.java
@@ -14,7 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.krawen.movieservice.dto.SearchMovieResponseDTO;
 import com.krawen.movieservice.dto.SearchPersonResponseDTO;
+import com.krawen.movieservice.external.service.SearchMovieResponse;
 import com.krawen.movieservice.external.service.dto.SearchPersonResponse;
 import com.krawen.movieservice.properties.MovieServiceProperties;
 
@@ -32,16 +34,35 @@ class ExternalMovieServiceImplTest {
 
 	ResponseEntity<SearchPersonResponse> searchPersonResponseEntity;
 	SearchPersonResponse searchPersonResponse;
+	ResponseEntity<SearchMovieResponse> searchMovieResponseEntity;
+	SearchMovieResponse searchMovieResponse;
 
 	String timeWindow = "day";
-
+	int page = 1;
+	String withGenres="Action";
+	
 	@BeforeEach
 	void setup() {
 		searchPersonResponse = new SearchPersonResponse();
 		searchPersonResponse.setPage(1);
 		searchPersonResponseEntity = new ResponseEntity<SearchPersonResponse>(searchPersonResponse, HttpStatus.OK);
 		when(movieServiceProperties.getMovieApiBearerToken()).thenReturn("");
+		searchMovieResponse = new SearchMovieResponse();
+		searchMovieResponse.setPage(1L);
+		searchMovieResponseEntity = new ResponseEntity<SearchMovieResponse>(searchMovieResponse, HttpStatus.OK);
 	}
+	
+	@Test
+    void testDiscoverMovieSuccess() {
+    	
+        when(restTemplate.exchange(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.<Class<SearchMovieResponse>>any()))
+             .thenReturn(searchMovieResponseEntity);
+
+        SearchMovieResponseDTO discoverMovieResponse = extMovieService.discoverMovie(withGenres,page);
+        assertEquals(discoverMovieResponse.getPage(), searchMovieResponseEntity.getBody().getPage());
+    }
 
 	@Test
     void testRetrieveTrendingPeopleSuccess() {
